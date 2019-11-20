9a756556f7a4486afa65f24d3cf3ade9af545a98
Daniel Hernandez <danielmark252525@gmail.com>
2019-11-20 17:51:58 -0500

    Adds More

--- mail.php
+++ mail.php
@@ -43,7 +43,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "example@yourdomain.com";
+$address = "bamcamexicangrill@gmail.com";
 
 
 // Configuration option.
@@ -86,3 +86,5 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo 'ERROR!';
 
 }
+
+?>
